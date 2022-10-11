720a6b2e09f3cf8be14bd390f6a9905a29d08f0e
Lester Jones <lesterjones95@gmail.com>
2022-10-11 17:25:08 +0200

    first commit

---
+++ src/main/java/com/nostresswithles/turbofanapi/TurbofanApiApplication.java
@@ -0,0 +1,13 @@
+package com.nostresswithles.turbofanapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurbofanApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurbofanApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nostresswithles/turbofanapi/TurbofanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nostresswithles.turbofanapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurbofanApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
