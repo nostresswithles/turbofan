3166406ef9467ee4c1031a4e1754e13fea99f37a
Lester Jones <lesterjones95@gmail.com>
2022-10-11 19:03:52 +0200

    created index html

--- src/main/java/com/nostresswithles/turbofanapi/controllers/GreetingController.java
+++ src/main/java/com/nostresswithles/turbofanapi/controllers/GreetingController.java
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class IndexController {
+public class GreetingController {
 
-    @GetMapping("/index")
+    @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
-        return "index";
+        return "greeting";
     }
 
 }
